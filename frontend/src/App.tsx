75024a24d7067411e718ec3baf1858347148c712
Andy Pfister <andy.pfister@simplificator.com>
2020-09-12 15:52:00 +0200

    Add very basic frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3 className="title is-3">Netdata alarms</h3>
+      <table className="table is-bordered">
+        <thead>
+          <tr>
+            <th>System</th>
+            <th>Alarm</th>
+            <th>Last status change</th>
+            <th>Current value</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </>
   );
 }
 
